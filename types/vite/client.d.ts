2c317cf3fa96233ac209e2935f0ccfc0243bdb56
nexus <82994528+Staticnoi@users.noreply.github.com>
2026-05-28 14:59:52 +0700

    Fixed some errors

--- types/vite/client.d.ts
+++ types/vite/client.d.ts
@@ -1,7 +1,16 @@
 interface ImportMetaEnv {
+  readonly BASE_URL: string;
   readonly MODE: string;
   readonly DEV: boolean;
   readonly PROD: boolean;
+  readonly VITE_API_URL?: string;
+  readonly VITE_FIREBASE_API_KEY?: string;
+  readonly VITE_FIREBASE_AUTH_DOMAIN?: string;
+  readonly VITE_FIREBASE_PROJECT_ID?: string;
+  readonly VITE_FIREBASE_STORAGE_BUCKET?: string;
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID?: string;
+  readonly VITE_FIREBASE_APP_ID?: string;
+  readonly VITE_FIREBASE_VAPID_KEY?: string;
   readonly [key: string]: string | boolean | undefined;
 }
 
